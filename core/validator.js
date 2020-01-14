c2dca39bd25e24367bddd551fbdcea24e9d13050
boom <510126766@qq.com>
2020-01-14 18:13:40 +0800

    Update validator.js
    修改bug

--- core/validator.js
+++ core/validator.js
@@ -1,35 +1,48 @@
 const validator = require('validator')
 const { ParameterException } = require('./httpException')
 
+
 class Validator {
   constructor(rules) {
-    this.rules = rules
-    this.rule()
+    if (!Validator.instance) {
+      this.rules = rules;
+      this.rule();
+      Validator.instance = this;
+    }
+    return Validator.instance;
+  }
+  init(rules) {
+    this.rules = rules;
+    this.rule();
   }
   rule() {
     this.rules.forEach(v => {
+      if (v.isOptional == null) {
+        v.isOptional = true;
+      }
       if (v.isOptional && v.field) {
-        Validator.exception(v)
+        Validator.exception(v);
       }
-      if (!v.isOptional) {
-        Validator.exception(v)
+      if (v.isOptional && !v.field) {
+        Validator.exception(v);
       }
-    })
+    });
   }
   static exception(v) {
-    if (v.type) {
+    const errMsg = typeof v.errMsg !== "string" ? "" : v.errMsg;
+    if (Array.isArray(v.rules) && v.rules.length) {
       v.rules.forEach(iv => {
-        if (!validator[iv.type](v.field + '', iv.param)) {
-          throw new ParameterException()
+        if (!validator[iv.type](v.field + "", iv.param)) {
+          throw new ParameterException(errMsg);
         }
-      })
+      });
     }
     if (v.custom && !v.custom()) {
-      throw new ParameterException()
+      throw new ParameterException(errMsg);
     }
   }
 }
 
 module.exports = {
   Validator
-}
+};
